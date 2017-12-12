972f496455155dda058366f89b3a54ba8cf5356a
Derek Miranda <derek.miranda@ff0000.com>
2017-12-12 14:16:08 -0800

    Init w/ top-level lib files done

---
+++ index.js
@@ -0,0 +1,33 @@
+import AlphaVideoPlayer from './AlphaVideoPlayer'
+import AudioPlayer from './AudioPlayer'
+import VideoControls from './VideoControls'
+import VideoPlayer from './VideoPlayer'
+import YouTubePlayer from './YouTubePlayer'
+import MediaEvent from './events/MediaEvent'
+import UIButtonFullScreen from './ui/UIButtonFullScreen'
+import UIButtonMute from './ui/UIButtonMute'
+import UIButtonPlayPause from './ui/UIButtonPlayPause'
+import UIButtonReplay from './ui/UIButtonReplay'
+import UIControlBar from './ui/UIControlBar'
+import UIPoster from './ui/UIPoster'
+import UISliderProgress from './ui/UISliderProgress'
+import UISliderVolume from './ui/UISliderVolume'
+import UITimeDisplay from './ui/UITimeDisplay'
+
+export {
+	AlphaVideoPlayer,
+	AudioPlayer,
+	VideoControls,
+	VideoPlayer,
+	YouTubePlayer,
+	MediaEvent,
+	UIButtonFullScreen,
+	UIButtonMute,
+	UIButtonPlayPause,
+	UIButtonReplay,
+	UIControlBar,
+	UIPoster,
+	UISliderProgress,
+	UISliderVolume,
+	UITimeDisplay,
+}
